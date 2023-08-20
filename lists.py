97ebad2c37940a0f1153c50f8d036067ac05a419
Bhushan Chaudhari <68490983+bhush-n@users.noreply.github.com>
2023-08-20 23:56:47 +0530

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,13 +1,13 @@
 list1 = ["B", "H", "U", "S", "H", "A", "N"]
 print(type(list1))
 
-# list1.remove("H") # Remove the given element
-# list1.sort() # Sort the list
-# list1.append(23)  # adds the element in the list
-# list1.extend([2, 3, 6, 4])  # adds the whole list in the given list
-# list1.pop()  # Removes the last element of list
-# print(list1.index("N"))  # Print the indexing of an element
-# list1.reverse()  #  It reverse the list
-# list1.insert(3, 8)  #  It will add an element on the index no.
-# print(list1[::-1])  #  It will slice the list and print the list in reverse order
+list1.remove("H") # Remove the given element
+list1.sort() # Sort the list
+list1.append(23)  # adds the element in the list
+list1.extend([2, 3, 6, 4])  # adds the whole list in the given list
+list1.pop()  # Removes the last element of list
+print(list1.index("N"))  # Print the indexing of an element
+list1.reverse()  #  It reverse the list
+list1.insert(3, 8)  #  It will add an element at the index no.
+print(list1[::-1])  #  It will slice the list and print the list in reverse order
 
