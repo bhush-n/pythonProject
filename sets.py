f0404dcd5790ae96a25f66e68b46cea83a7ea0ad
Bhushan Chaudhari <68490983+bhush-n@users.noreply.github.com>
2023-08-20 23:57:22 +0530

    Update sets.py

--- sets.py
+++ sets.py
@@ -1,11 +1,11 @@
 a1 = {3, 5, 6, 3, 4, 8}
 a2 = {3, 5, 6, 3, 10, 11, 12, 16}
 
-# a1.clear()  # It will clear the set
-# a1.pop()  # It will remove the random element from set
-# a1.add(9)  # It will add an element in set
-# print(a1.union(a2))  # It will merge the a2 set into a1 and return a new set
-# print(a1.intersection(a2))  # It will return the common elements between two sets with a new set
+a1.clear()  # It will clear the set
+a1.pop()  # It will remove the random element from set
+a1.add(9)  # It will add an element in set
+print(a1.union(a2))  # It will merge the a2 set into a1 and return a new set
+print(a1.intersection(a2))  # It will return the common elements between two sets with a new set
 
 print(a1)
 print(a2)
